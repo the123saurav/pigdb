d79f8d80ad37fd134ebd0bdc33cf43ad35c74064
the123saurav <42906612+the123saurav@users.noreply.github.com>
2024-10-10 21:50:06 -0700

    Add buffer pool getpage without eviction (#10)

--- src/buffer_pool.h
+++ src/buffer_pool.h
@@ -3,11 +3,12 @@
 
 #include "core.h"
 #include "disk-manager.h"
-#include <_types/_uint32_t.h>
-#include <_types/_uint64_t.h>
+#include "error.h"
+#include "lock_free_stack.h"
 #include <array>
 #include <atomic>
 #include <cstddef>
+#include <cstdint>
 #include <memory>
 #include <shared_mutex>
 #include <unordered_map>
@@ -35,27 +36,69 @@ namespace Pig {
         If it can't it evicts a page, if dirty flushing it to disk
         synchronously.
          */
-        class BufferPool {
-          public:
-            BufferPool(size_t                       numFrames,
-                       std::shared_ptr<DiskManager> diskManager);
-
+        class BufferPool : public std::enable_shared_from_this<BufferPool> {
           private:
             struct Frame {
                 std::atomic_uint16_t m_pinCount;
                 std::atomic_bool     m_dirty;
                 // page in the buffer pool, not interpreted by buffer pool.
                 unsigned char m_page[PAGE_SIZE_B];
+
+                Frame() : m_pinCount{0}, m_dirty{false} {}
             };
 
-            void Evict();
+            class BufferPoolPageGuard {
+
+              public:
+                BufferPoolPageGuard(Frame &f) : m_frame(f) {
+                    m_frame.m_pinCount++;
+                }
+
+                BufferPoolPageGuard(const BufferPoolPageGuard &) = delete;
+                BufferPoolPageGuard(BufferPoolPageGuard &&)      = delete;
+
+                BufferPoolPageGuard &
+                operator=(const BufferPoolPageGuard &)                 = delete;
+                BufferPoolPageGuard &operator=(BufferPoolPageGuard &&) = delete;
+
+                ~BufferPoolPageGuard() { m_frame.m_pinCount--; }
+
+                void markDirty() { m_frame.m_dirty.store(true); }
+
+              private:
+                Frame &m_frame;
+            };
+
+          public:
+            BufferPool(size_t                       numFrames,
+                       std::shared_ptr<DiskManager> diskManager);
+
+            /**
+                Gets a page from bufferpool.
+                If the page is in pool, its latest.
+                If the page is not present, tries to reserve a frame from
+                free list and then reads it from disk assuming the page is
+                valid. If there are no frames, it evicts the first unpinned page
+                from LRU.
+             */
+            BufferPoolPageGuard GetPage(IoId_t io_id, page_id_t page_id);
+
+          private:
+            void evictPage();
+
+            [[nodiscard]] Error flushPage(IoId_t io_id, page_id_t page_id);
+
+            [[nodiscard]] Error readPageFromDisk(IoId_t    io_id,
+                                                 page_id_t page_id,
+                                                 iovec     buffer) const;
 
             std::shared_ptr<DiskManager> m_diskManager;
 
-            std::shared_mutex                              m_mutex;
+            std::shared_mutex m_mutex;
+            // FrameId_t is index in m_frames.
             std::unordered_map<BufferPoolKey_t, FrameId_t> m_map;
-            const std::vector<Frame>                       m_frames;
-            lock_free_stack<FrameId_t>                     m_freeFrames;
+            std::vector<Frame>                             m_frames;
+            LockFreeStack<FrameId_t>                       m_freeFrames;
         };
     } // namespace Core
 
--- src/core.h
+++ src/core.h
@@ -9,9 +9,10 @@ namespace Pig {
         using page_id_t   = uint16_t;
         using page_size_t = uint16_t;
 
-        constexpr uint8_t  PAGE_SIZE_KB = 4;
-        constexpr uint16_t MAX_PAGES    = 32768; // 4KB page enough for spacemap
-        constexpr uint16_t MAX_TABLES   = 100;
+        constexpr uint8_t     PAGE_SIZE_KB = 4;
+        constexpr page_size_t PAGE_SIZE_B  = PAGE_SIZE_KB * 1024;
+        constexpr uint16_t    MAX_PAGES = 32768; // 4KB page enough for spacemap
+        constexpr uint16_t    MAX_TABLES = 100;
     } // namespace Core
 } // namespace Pig
 
